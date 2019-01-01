075384696d6d7d083d095392ed01f5a6e9be8930
amarteragundi <amar@iweavesolutions.com>
2019-01-01 15:14:18 +0530

    Update import.js

--- import.js
+++ import.js
@@ -1,1 +1,3 @@
 https://medium.com/@rogerdudler/front-end-technology-stack-survey-2014-809f7a8c92f3
+
+https://hackernoon.com/i-finally-understand-static-vs-dynamic-typing-and-you-will-too-ad0c2bd0acc7
